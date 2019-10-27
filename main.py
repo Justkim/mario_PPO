2de1f17a630ffbe0699fcc6332d5780ba135e7fe
Justkim <kimiya.saadat97@gmail.com>
2019-10-27 13:56:36 +0330

    changed save interval

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def main():
         env = mario_env.make_train_0()
 
     if flag.TRAIN:
-        new_trainer=Trainer(num_training_steps=20000,num_epoch=2,batch_size=16,learning_rate=0.0001
-                            ,discount_factor=0.99,env=env,num_action=7,save_interval=50,log_interval=50,decay_rate=0.001,num_steps=16,memory_size=100)
+        new_trainer=Trainer(num_training_steps=200000,num_epoch=2,batch_size=16,learning_rate=0.0001
+                            ,discount_factor=0.99,env=env,num_action=7,save_interval=100,log_interval=50,decay_rate=0.001,num_steps=16,memory_size=100)
         new_trainer.collect_experience_and_train()
     elif flag.PLAY:
         new_player=Player(env=env,load_path='')
