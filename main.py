6158eba8d2282f88947767800a8f65a1195d31db
Justkim <kimiya.saadat97@gmail.com>
2019-11-10 21:41:08 +0330

    reduced envs

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ else:
     env = gym_minigrid.wrappers.StateBonus(env)
 
 if flag.TRAIN:
-    new_trainer=Trainer(num_training_steps=20000,num_env=16,num_game_steps=128,num_epoch=3,learning_rate=0.001
+    new_trainer=Trainer(num_training_steps=20000,num_env=1,num_game_steps=128,num_epoch=3,learning_rate=0.001
                         ,discount_factor=0.99,env=env,num_action=5,clip_range=0.1,value_coef=0.5,save_interval=50,
                         log_interval=10,
-                        entropy_coef=0.0,lam=0.99,mini_batch_size=256,num_action_repeat=1)
+                        entropy_coef=0.0,lam=0.99,mini_batch_size=64,num_action_repeat=1)
     new_trainer.collect_experiance_and_train()
 elif flag.PLAY:
     new_player=Player(env=env,load_path='./trains/3/step1100-20191107-120331/train')
