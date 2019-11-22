a9ea7978c5d66b77bfe886a8f64c5f8726c37498
Justkim <kimiya.saadat97@gmail.com>
2019-11-22 10:42:49 +0330

    changed mini batch size

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ if flag.TRAIN:
     new_trainer=Trainer(num_training_steps=20000,num_env=2,num_game_steps=8,num_epoch=3,learning_rate=0.00025
                         ,discount_factor=0.99,env=env,num_action=5,clip_range=0.2,value_coef=0.5,save_interval=50,
                         log_interval=10,
-                        entropy_coef=0.02,lam=0.95,mini_batch_size=8,num_action_repeat=1)
+                        entropy_coef=0.02,lam=0.95,mini_batch_size=16,num_action_repeat=1)
     new_trainer.collect_experiance_and_train()
 elif flag.PLAY:
     env = moving_dot_env.make_train_0()
-    new_player=Player(env=env,load_path='/home/kim/DeepRL_Palace/my_DeepRl_projects/mario_PPO/models/step9300-20191121-112302/train')
+    new_player=Player(env=env,load_path='/home/kim/DeepRL_Palace/my_DeepRl_projects/mario_PPO/trains/10/step9500-20191121-135731/train')
     new_player.play()
 
 # else:
