46c636cf4e5dad352bb61a201dc2b5342352c468
Justkim <kimiya.saadat97@gmail.com>
2019-11-29 14:05:31 +0330

    removed unused dependency

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 from train import Trainer
-from env import *
-import mario_env
-import gym
 import flag
 from play import Player
 import moving_dot_env
 
 
-if flag.MARIO_ENV:
-    env=mario_env.make_train_0()
-else:
-    env = gym.make('MiniGrid-Empty-5x5-v0')
-    env = gym_minigrid.wrappers.RGBImgObsWrapper(
-        env)  # attention: I changed render parameters in wrappers file to make this work
-
-    # env=env.make_train_0()
-    env = processObservation(env, (86, 86))
-    env = gym_minigrid.wrappers.StateBonus(env)
-
 if flag.TRAIN:
-    new_trainer=Trainer(num_training_steps=20000,num_env=12,num_game_steps=128,num_epoch=4,learning_rate=2e-4
-                        ,discount_factor=0.99,env=env,num_action=5,clip_range=0.2,value_coef=0.5,save_interval=20,
+    new_trainer=Trainer(num_training_steps=20000,num_env=1,num_game_steps=8,num_epoch=2,learning_rate=2e-4
+                        ,discount_factor=0.99,env=env,num_action=5,clip_range=0.1,value_coef=0.5,save_interval=20,
                         log_interval=10,
                         entropy_coef=0.05,lam=0.95,mini_batch_size=8,num_action_repeat=1)
     new_trainer.collect_experiance_and_train()
