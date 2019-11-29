64b8135a476ecc38c8e07678563c2fbadbf2b493
Justkim <kimiya.saadat97@gmail.com>
2019-11-29 14:07:35 +0330

    removed unused dependency

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import moving_dot_env
 
 if flag.TRAIN:
     new_trainer=Trainer(num_training_steps=20000,num_env=1,num_game_steps=8,num_epoch=2,learning_rate=2e-4
-                        ,discount_factor=0.99,env=env,num_action=5,clip_range=0.1,value_coef=0.5,save_interval=20,
+                        ,discount_factor=0.99,num_action=5,clip_range=0.1,value_coef=0.5,save_interval=20,
                         log_interval=10,
                         entropy_coef=0.05,lam=0.95,mini_batch_size=8,num_action_repeat=1)
     new_trainer.collect_experiance_and_train()
