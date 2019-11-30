ed3163fca303b69c92f7f878295cf507806b4c23
Justkim <kimiya.saadat97@gmail.com>
2019-11-30 10:24:50 +0330

    solved the syntax error

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import moving_dot_env
 
 if flag.TRAIN:
     new_trainer = Trainer(num_training_steps=20000, num_env=12, num_game_steps=128, num_epoch=4, learning_rate=2e-4
-                          , discount_factor=0.99, env=env, num_action=5, clip_range=0.2, value_coef=0.5,
+                          , discount_factor=0.99, num_action=5, clip_range=0.2, value_coef=0.5,
                           save_interval=20,
                           log_interval=10,
                           entropy_coef=0.05, lam=0.95, mini_batch_size=8, num_action_repeat=1)
