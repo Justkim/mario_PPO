4fc658a3b9fe8656c5e6aa0596260c0dd2907560
Justkim <kimiya.saadat97@gmail.com>
2019-11-30 10:53:38 +0330

    changed hyperparameters

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import moving_dot_env
 
 
 if flag.TRAIN:
-    new_trainer = Trainer(num_training_steps=20000, num_env=12, num_game_steps=128, num_epoch=4, learning_rate=2e-4
+    new_trainer = Trainer(num_training_steps=20000, num_env=6, num_game_steps=32, num_epoch=4, learning_rate=2e-4
                           , discount_factor=0.99, num_action=5, clip_range=0.2, value_coef=0.5,
                           save_interval=20,
                           log_interval=10,
-                          entropy_coef=0.05, lam=0.95, mini_batch_size=8, num_action_repeat=1)
+                          entropy_coef=0.05, lam=0.95, mini_batch_size=32*6, num_action_repeat=1)
     new_trainer.collect_experiance_and_train()
 elif flag.PLAY:
     env = moving_dot_env.make_train_0()
