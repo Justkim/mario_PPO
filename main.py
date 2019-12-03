986ea02bf8528b1eb4b6feeaf22216404f099ddd
Justkim <kimiya.saadat97@gmail.com>
2019-12-03 13:46:11 +0330

    increased sasve interval

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import moving_dot_env
 if flag.TRAIN:
     new_trainer = Trainer(num_training_steps=20000, num_env=1, num_game_steps=16, num_epoch=4, learning_rate=1e-4
                           , discount_factor=0.99, num_action=5, clip_range=0.2, value_coef=0.5,
-                          save_interval=20,
+                          save_interval=50,
                           log_interval=10,
                           entropy_coef=0.05, lam=0.95, mini_batch_size=16, num_action_repeat=1)
     new_trainer.collect_experiance_and_train()
